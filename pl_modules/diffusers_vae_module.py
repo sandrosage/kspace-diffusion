60b16c3f52deb8826ee08654f7d53d1bb8591a45
sandrosage <sandrosage@gmx.de>
2025-06-16 13:10:30 +0200

    upload workstation state with diffusers vae

---
+++ pl_modules/diffusers_vae_module.py
@@ -0,0 +1,107 @@
+from pl_modules.mri_module import NewMRIModule
+from diffusers.models.autoencoders.vae import Decoder, Encoder
+from torch.nn import L1Loss
+import torch
+from modules.transforms import KspaceUNetSample, norm, unnorm, kspace_to_mri
+
+def create_channels(n: int, chans: int = 32):
+    blocks = (chans,)
+    for i in range(n-1):
+        blocks += (2*chans,)
+        chans = 2*chans
+    return blocks
+
+class Diffusers_VAE(NewMRIModule):
+    def __init__(self, 
+                 in_channels: int = 2, 
+                 out_channels: int = 2, 
+                 latent_dim: int = 16,
+                 down_layers: int = 4,  
+                 num_log_images = 32):
+        super().__init__(num_log_images)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.latent_dim = latent_dim
+        self.down_layers = down_layers
+
+        down_block_out_channels = create_channels(self.down_layers)
+        up_block_out_channels = create_channels(self.down_layers)[::-1]
+        down_blocks = self.down_layers*("DownEncoderBlock2D", )
+        up_blocks = self.down_layers* ("UpEncoderBlock2D", )
+
+        self.encoder = Encoder(
+            in_channels=self.in_channels, 
+            out_channels=self.latent_dim, 
+            down_block_types=down_blocks, 
+            block_out_channels=down_block_out_channels,
+            double_z=False)
+        
+        self.decoder = Decoder(
+            in_channels=self.latent_dim,
+            out_channels=self.out_channels,
+            up_block_types=up_blocks,
+            block_out_channels=up_block_out_channels
+        )
+
+        self.criterion = L1Loss()
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        z = self.encoder(x)
+        return self.decoder(z)
+    
+    def encode(self, x: torch.Tensor) -> torch.Tensor:
+        return self.encoder(x)
+    
+    def decode(self, z: torch.Tensor) -> torch.Tensor:
+        return self.decoder(z)
+    
+    def shared_step(self, batch: KspaceUNetSample):
+        input = batch.full_kspace
+        input = input.permute(0,3,1,2).contiguous()
+        input, mean, std = norm(input)
+        output = self(input)
+        loss = self.criterion(input,output)
+        output = unnorm(output, mean, std)
+        output = output.permute(0,2,3,1).contiguous()
+        return loss, output
+    
+    def training_step(self, batch: KspaceUNetSample, batch_idx):
+        loss, output = self.shared_step(batch)
+
+        input_img = kspace_to_mri(batch.full_kspace)
+        output_img = kspace_to_mri(output)
+
+        img_loss = self.criterion(input_img, output_img)
+
+        self.log_dict({
+            "train/kspace_l1": loss, 
+            "train/img_l1": img_loss
+        }, on_epoch=True, on_step=True, sync_dist=True, prog_bar=True, batch_size=batch.full_kspace.shape[0])
+
+        return {
+            "loss": loss,
+            "undersampled_mri_image": input_img,
+            "reconstructed_mri_image": output_img, 
+            "full_mri_image": batch.target
+        }
+
+    def validation_step(self, batch:KspaceUNetSample, batch_idx):
+        loss, output = self.shared_step(batch)
+    
+        input_img = kspace_to_mri(batch.full_kspace)
+        output_img = kspace_to_mri(output)
+
+        img_loss = self.criterion(input_img, output_img)
+
+        self.log_dict({
+            "val/kspace_l1": loss, 
+            "val/img_l1": img_loss
+        }, on_epoch=True, on_step=True, sync_dist=True, prog_bar=True, batch_size=batch.full_kspace.shape[0])
+        
+        return {
+            "loss": loss,
+            "undersampled_mri_image": input_img,
+            "reconstructed_mri_image": output_img, 
+            "full_mri_image": batch.target
+        }
--- student_teacher.py
+++ student_teacher.py
@@ -15,89 +15,89 @@ from fastmri.data.subsample import create_mask_for_mask_type
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-class StudentNoSkipUnet(NewMRIModule):
-    def __init__(self, path: str, num_log_images = 128):
-        super().__init__(num_log_images)
-        self.teacher = MyUnetModule.load_from_checkpoint(path)
-        self.teacher.eval()
-        self.teacher.model.eval()
-        for param in self.teacher.parameters():
-            param.requires_grad = False
-        self.lr = 0.0003
-        self.lr_step_size = 40
-        self.lr_gamma = 0.1
-        self.weight_decay = 0.0
-        self.model = Unet_FastMRI(2, 2, 128, with_residuals=False)
+# class StudentNoSkipUnet(NewMRIModule):
+#     def __init__(self, path: str, num_log_images = 128):
+#         super().__init__(num_log_images)
+#         self.teacher = MyUnetModule.load_from_checkpoint(path)
+#         self.teacher.eval()
+#         self.teacher.model.eval()
+#         for param in self.teacher.parameters():
+#             param.requires_grad = False
+#         self.lr = 0.0003
+#         self.lr_step_size = 40
+#         self.lr_gamma = 0.1
+#         self.weight_decay = 0.0
+#         self.model = Unet_FastMRI(2, 2, 128, with_residuals=False)
     
-    def norm(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        # group norm
-        b, c, h, w = x.shape
-        x = x.view(b, 2, c // 2 * h * w)
-        mean = x.mean(dim=2).view(b, 2, 1, 1)
-        std = x.std(dim=2).view(b, 2, 1, 1)
+#     def norm(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+#         # group norm
+#         b, c, h, w = x.shape
+#         x = x.view(b, 2, c // 2 * h * w)
+#         mean = x.mean(dim=2).view(b, 2, 1, 1)
+#         std = x.std(dim=2).view(b, 2, 1, 1)
 
-        x = x.view(b, c, h, w)
+#         x = x.view(b, c, h, w)
 
-        return (x - mean) / std, mean, std
+#         return (x - mean) / std, mean, std
 
-    def unnorm(self, x: torch.Tensor, mean: torch.Tensor, std: torch.Tensor) -> torch.Tensor:
-        return x * std + mean
+#     def unnorm(self, x: torch.Tensor, mean: torch.Tensor, std: torch.Tensor) -> torch.Tensor:
+#         return x * std + mean
     
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        output = self.model(input)
-        return output
+#     def forward(self, input: torch.Tensor) -> torch.Tensor:
+#         output = self.model(input)
+#         return output
     
-    def shared_step(self, batch: KspaceUNetSample) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        full_kspace = batch.full_kspace
-        masked_kspace = batch.masked_kspace
-        mask = batch.mask
-        full_mri_image = kspace_to_mri(full_kspace, (320,320))
-        undersampled_mri_image = kspace_to_mri(masked_kspace, (320,320))
-        return full_kspace, masked_kspace, full_mri_image, undersampled_mri_image
+#     def shared_step(self, batch: KspaceUNetSample) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+#         full_kspace = batch.full_kspace
+#         masked_kspace = batch.masked_kspace
+#         mask = batch.mask
+#         full_mri_image = kspace_to_mri(full_kspace, (320,320))
+#         undersampled_mri_image = kspace_to_mri(masked_kspace, (320,320))
+#         return full_kspace, masked_kspace, full_mri_image, undersampled_mri_image
     
-    def training_step(self, batch: KspaceUNetSample, batch_idx):
-        full_kspace, masked_kspace, full_mri_image, _ = self.shared_step(batch)
-        input = masked_kspace.permute(0,3,1,2).contiguous()
-        input, mean, std = self.norm(input)
-        # teacher output generation
-        z, stack = self.teacher.downsample(input)
-        teacher_output = self.teacher.upsample(z, stack)
+#     def training_step(self, batch: KspaceUNetSample, batch_idx):
+#         full_kspace, masked_kspace, full_mri_image, _ = self.shared_step(batch)
+#         input = masked_kspace.permute(0,3,1,2).contiguous()
+#         input, mean, std = self.norm(input)
+#         # teacher output generation
+#         z, stack = self.teacher.downsample(input)
+#         teacher_output = self.teacher.upsample(z, stack)
 
-        # student output generation
-        student_output = self(input)
+#         # student output generation
+#         student_output = self(input)
 
-        teacher_output = self.unnorm(teacher_output, mean, std)
-        student_output = self.unnorm(student_output, mean, std)
+#         teacher_output = self.unnorm(teacher_output, mean, std)
+#         student_output = self.unnorm(student_output, mean, std)
 
-        teacher_output = teacher_output.permute(0,2,3,1).contiguous()
-        student_output = student_output.permute(0,2,3,1).contiguous()
+#         teacher_output = teacher_output.permute(0,2,3,1).contiguous()
+#         student_output = student_output.permute(0,2,3,1).contiguous()
 
-        teacher_mri_output = kspace_to_mri(teacher_output, (320,320))
-        student_mri_output = kspace_to_mri(student_output, (320,320))
-        loss = self.reconstruction_distillation_loss(student_mri_output, teacher_mri_output, full_mri_image)
-        self.log("train/dist_loss", loss, on_epoch=True, on_step=True, prog_bar=True, batch_size=full_kspace.shape[0], sync_dist=True)
-        return {
-            "loss": loss, 
-            "undersampled_mri_image": teacher_mri_output,
-            "reconstructed_mri_image": student_mri_output,
-            "full_mri_image": full_mri_image
-        }
+#         teacher_mri_output = kspace_to_mri(teacher_output, (320,320))
+#         student_mri_output = kspace_to_mri(student_output, (320,320))
+#         loss = self.reconstruction_distillation_loss(student_mri_output, teacher_mri_output, full_mri_image)
+#         self.log("train/dist_loss", loss, on_epoch=True, on_step=True, prog_bar=True, batch_size=full_kspace.shape[0], sync_dist=True)
+#         return {
+#             "loss": loss, 
+#             "undersampled_mri_image": teacher_mri_output,
+#             "reconstructed_mri_image": student_mri_output,
+#             "full_mri_image": full_mri_image
+#         }
     
-    def configure_optimizers(self):
-        optim = torch.optim.Adam(
-            self.parameters(), lr=self.lr, weight_decay=self.weight_decay
-        )
-        scheduler = torch.optim.lr_scheduler.StepLR(
-            optim, self.lr_step_size, self.lr_gamma
-        )
+#     def configure_optimizers(self):
+#         optim = torch.optim.Adam(
+#             self.parameters(), lr=self.lr, weight_decay=self.weight_decay
+#         )
+#         scheduler = torch.optim.lr_scheduler.StepLR(
+#             optim, self.lr_step_size, self.lr_gamma
+#         )
 
-        return [optim], [scheduler]
+#         return [optim], [scheduler]
 
-    def reconstruction_distillation_loss(self, student_output, teacher_output, target, alpha=0.5):
-        # recon_loss = F.mse_loss(student_output, target)
-        distill_loss = F.l1_loss(student_output, teacher_output)
-        # return alpha * distill_loss + (1 - alpha) * recon_loss
-        return distill_loss
+#     def reconstruction_distillation_loss(self, student_output, teacher_output, target, alpha=0.5):
+#         # recon_loss = F.mse_loss(student_output, target)
+#         distill_loss = F.l1_loss(student_output, teacher_output)
+#         # return alpha * distill_loss + (1 - alpha) * recon_loss
+#         return distill_loss
 
 
 if __name__ == "__main__":
@@ -114,20 +114,20 @@ if __name__ == "__main__":
         "mode": "interpolation"
     }
 
-    model_name = "StudentNoSkipUnet"
-    run_name = model_name + datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    wandb.login(key="c210746318a0cf3a3fb1d542db1864e0a789e94c")
-    wandb_logger = WandbLogger(project="Kspace-Experiments", name=run_name, log_model=True, config=config)
+#     model_name = "StudentNoSkipUnet"
+#     run_name = model_name + datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+#     wandb.login(key="c210746318a0cf3a3fb1d542db1864e0a789e94c")
+#     wandb_logger = WandbLogger(project="Kspace-Experiments", name=run_name, log_model=True, config=config)
 
-    model = StudentNoSkipUnet(path="6jxi7pym/myunet-epoch=01.ckpt")
+#     model = StudentNoSkipUnet(path="6jxi7pym/myunet-epoch=01.ckpt")
 
-    model_checkpoint = ModelCheckpoint(
-        save_top_k=2,
-        monitor="val/ssim_loss_epoch",
-        mode="min",
-        filename="myunet-{epoch:02d}"
-    )
-    trainer = pl.Trainer(devices=1, max_epochs=2, logger=wandb_logger, callbacks=[model_checkpoint])
+#     model_checkpoint = ModelCheckpoint(
+#         save_top_k=2,
+#         monitor="val/ssim_loss_epoch",
+#         mode="min",
+#         filename="myunet-{epoch:02d}"
+#     )
+#     trainer = pl.Trainer(devices=1, max_epochs=2, logger=wandb_logger, callbacks=[model_checkpoint])
 
     mask_func = create_mask_for_mask_type(
         config["mask_type"], config["center_fractions"], config["accelerations"]
@@ -151,62 +151,62 @@ if __name__ == "__main__":
         distributed_sampler=False,
         use_dataset_cache_file=True
     )
-    trainer.fit(model,data_module.train_dataloader())
-    wandb.finish()
-    model = MyUnetModule.load_from_checkpoint("6jxi7pym/myunet-epoch=01.ckpt")
-    # train_dataloader = data_module.train_dataloader()
-    # for i in range(5):
-    #     batch = next(iter(train_dataloader))
-    #     full_kspace = batch.full_kspace
-    #     masked_kspace = batch.masked_kspace
-    #     masked_kspace = masked_kspace.to("cuda")
-    #     mask = batch.mask
-    #     mask = mask.to("cuda")
-    #     input = masked_kspace.permute(0,3,1,2).contiguous()
-    #     input = input.to("cuda")
-    #     input, mean, std = model.norm(input)
-    #     z, stack = model.downsample(input)
-    #     output = model.upsample(z, stack)
-    #     output = model.unnorm(output, mean, std)
-    #     reconstructed_kspace = output.permute(0,2,3,1).contiguous()
-    #     zero = torch.zeros(1, 1, 1, 1).to("cuda")
-    #     soft_dc = torch.where(mask, reconstructed_kspace - masked_kspace, zero)
-    #     output = reconstructed_kspace - soft_dc
-    #     full_mri_image = kspace_to_mri(full_kspace, (320,320))
-    #     afterdc_mri_image = kspace_to_mri(output, (320,320))
-    #     reconstructed_mri_image = kspace_to_mri(reconstructed_kspace, (320,320))
-    #     masked_mri_image = kspace_to_mri(masked_kspace, (320,320))
-    #     masked_mri_image = masked_mri_image.squeeze(0).detach().cpu().numpy()
-    #     reconstructed_mri_image = reconstructed_mri_image.squeeze(0).detach().cpu().numpy()
-    #     afterdc_mri_image = afterdc_mri_image.squeeze(0).detach().cpu().numpy()
-    #     full_mri_image = full_mri_image.squeeze(0).detach().cpu().numpy()
-    #     fig, ax = plt.subplots(1,4,figsize=(18,5))
-    #     fig.subplots_adjust(wspace=0.0)
+#     trainer.fit(model,data_module.train_dataloader())
+#     wandb.finish()
+    model = MyUnetModule.load_from_checkpoint("6fmyvljd/myunet-epoch=01.ckpt")
+    train_dataloader = data_module.train_dataloader()
+    for i in range(5):
+        batch = next(iter(train_dataloader))
+        full_kspace = batch.full_kspace
+        masked_kspace = batch.masked_kspace
+        masked_kspace = masked_kspace.to("cuda")
+        mask = batch.mask
+        mask = mask.to("cuda")
+        input = masked_kspace.permute(0,3,1,2).contiguous()
+        input = input.to("cuda")
+        input, mean, std = model.norm(input)
+        z, stack = model.downsample(input)
+        output = model.upsample(z, stack)
+        output = model.unnorm(output, mean, std)
+        reconstructed_kspace = output.permute(0,2,3,1).contiguous()
+        zero = torch.zeros(1, 1, 1, 1).to("cuda")
+        soft_dc = torch.where(mask, reconstructed_kspace - masked_kspace, zero)
+        output = reconstructed_kspace - soft_dc
+        full_mri_image = kspace_to_mri(full_kspace, (320,320))
+        afterdc_mri_image = kspace_to_mri(output, (320,320))
+        reconstructed_mri_image = kspace_to_mri(reconstructed_kspace, (320,320))
+        masked_mri_image = kspace_to_mri(masked_kspace, (320,320))
+        masked_mri_image = masked_mri_image.squeeze(0).detach().cpu().numpy()
+        reconstructed_mri_image = reconstructed_mri_image.squeeze(0).detach().cpu().numpy()
+        afterdc_mri_image = afterdc_mri_image.squeeze(0).detach().cpu().numpy()
+        full_mri_image = full_mri_image.squeeze(0).detach().cpu().numpy()
+        fig, ax = plt.subplots(1,4,figsize=(18,5))
+        fig.subplots_adjust(wspace=0.0)
     
-    #     ax[0].imshow(full_mri_image,'gray')
-    #     ax[0].set_title("Full Mri Image")
+        ax[0].imshow(full_mri_image,'gray')
+        ax[0].set_title("Full Mri Image")
 
-    #     ax[1].imshow(reconstructed_mri_image,'gray')
-    #     ax[1].set_title("Reconstructed Mri Image")
+        ax[1].imshow(reconstructed_mri_image,'gray')
+        ax[1].set_title("Reconstructed Mri Image")
 
-    #     ax[2].imshow(afterdc_mri_image, 'gray')
-    #     ax[2].set_title("After dc Mri Image")
+        ax[2].imshow(afterdc_mri_image, 'gray')
+        ax[2].set_title("After dc Mri Image")
 
-    #     ax[3].imshow(masked_mri_image, 'gray')
-    #     ax[3].set_title("Undersampled Mri Image")
+        ax[3].imshow(masked_mri_image, 'gray')
+        ax[3].set_title("Undersampled Mri Image")
 
         
-    #     # remove all the ticks (both axes), and tick labels
-    #     for axes in ax:
-    #         axes.set_xticks([])
-    #         axes.set_yticks([])
-    #     # remove the frame of the chart
-    #     for axes in ax:
-    #         axes.spines['top'].set_visible(False)
-    #         axes.spines['right'].set_visible(False)
-    #         axes.spines['bottom'].set_visible(False)
-    #         axes.spines['left'].set_visible(False)
-    #     # remove the white space around the chart
-    #     plt.tight_layout()
-    #     plt.savefig(f"{i}.png")
-    # print("Done")
+        # remove all the ticks (both axes), and tick labels
+        for axes in ax:
+            axes.set_xticks([])
+            axes.set_yticks([])
+        # remove the frame of the chart
+        for axes in ax:
+            axes.spines['top'].set_visible(False)
+            axes.spines['right'].set_visible(False)
+            axes.spines['bottom'].set_visible(False)
+            axes.spines['left'].set_visible(False)
+        # remove the white space around the chart
+        plt.tight_layout()
+        plt.savefig(f"{i}.png")
+    print("Done")
